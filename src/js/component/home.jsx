27f76fd3b207c008315b9856ac6a03c443c14e34
Maximo1988 <max_bd88@hotmail.com>
2022-03-05 01:13:21 +0000

    añadiendo codigo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	//Logica
+	//[variable,funcion] = useState(valor por default)
+	const [nombre, setNombre] = useState("");
+	const [apellido, setApellido] = useState("");
+	const [lista, setLista] = useState(["tarea1", "tarea2", "tarea3"]);
+
+	console.log(lista);
+
+	const verificarNombre = () => {
+		if (nombre === "" && apellido === "") {
+			alert("Ingresa nombre y apellido");
+		} else if (nombre === "") {
+			alert("Ingrese nombre");
+		} else if (apellido === "") {
+			alert("Ingresa apellido");
+		} else {
+			alert("Puedes continuar");
+		}
+	};
+	//Logica
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="Fondo container mt-5">
+			<h3 className="todos">
+				{nombre} {apellido}
+			</h3>
+			<div className="container">
+				<label htmlFor="">Nombre</label>
+				<input
+					type="text"
+					placeholder="Ingresa tu nombre"
+					//      evento     setea        valor
+					onChange={(e) => setNombre(e.target.value)}
+					value={nombre}></input>
+				<input
+					type="text"
+					placeholder="Ingresa tu apellido"
+					onChange={(e) => {
+						setApellido(e.target.value);
+					}}
+					value={apellido}></input>
+				<button
+					className="btn btn-warning"
+					onClick={() => {
+						setNombre("");
+						setApellido("");
+					}}>
+					Limpiar input
+				</button>
+				<button
+					className="btn btn-success"
+					onClick={() => verificarNombre()}
+					onMouseOver>
+					Verificar
+				</button>
+				<button
+					className="btn btn-danger"
+					onClick={() => {
+						setLista(lista.concat(nombre));
+					}}>
+					Agregar a lista
+				</button>
+			</div>
+			{lista.map((tarea) => {
+				return <h1>{tarea}</h1>;
+			})}
 		</div>
 	);
 };
