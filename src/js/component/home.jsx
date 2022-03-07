ae0e3032256eebe22504e5e66930bc1fece1d6b1
Maximo1988 <max_bd88@hotmail.com>
2022-03-07 08:10:57 +0000

    actualizando codigo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,77 +1,80 @@
-import React, { useState } from "react";
+import React from "react";
+import { nanoid } from "nanoid";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function Home() {
+	const [tarea, setTarea] = React.useState("");
+	const [pendientes, setPendientes] = React.useState([]);
 
-//create your first component
-const Home = () => {
-	//Logica
-	//[variable,funcion] = useState(valor por default)
-	const [nombre, setNombre] = useState("");
-	const [apellido, setApellido] = useState("");
-	const [lista, setLista] = useState(["tarea1", "tarea2", "tarea3"]);
-
-	console.log(lista);
-
-	const verificarNombre = () => {
-		if (nombre === "" && apellido === "") {
-			alert("Ingresa nombre y apellido");
-		} else if (nombre === "") {
-			alert("Ingrese nombre");
-		} else if (apellido === "") {
-			alert("Ingresa apellido");
-		} else {
-			alert("Puedes continuar");
+	const agregarTarea = (e) => {
+		e.preventDefault();
+		if (!tarea.trim()) {
+			console.log("campo vacio");
+			return;
 		}
+
+		setPendientes([...pendientes, { id: nanoid(), NombreTarea: tarea }]);
+
+		setTarea("");
 	};
-	//Logica
+
+	function eliminarTarea(id) {
+		const arrayFilter = pendientes.filter((item) => item.id !== id);
+		setPendientes(arrayFilter);
+	}
+
+	const cantLista = () => {
+		let cantidad = 0;
+		cantidad = pendientes.length;
+		return cantidad;
+	};
+
+	let styleBox = {
+		width: "500px",
+		height: "auto",
+		margin: "auto",
+	};
+
 	return (
-		<div className="Fondo container mt-5">
-			<h3 className="todos">
-				{nombre} {apellido}
-			</h3>
-			<div className="container">
-				<label htmlFor="">Nombre</label>
-				<input
-					type="text"
-					placeholder="Ingresa tu nombre"
-					//      evento     setea        valor
-					onChange={(e) => setNombre(e.target.value)}
-					value={nombre}></input>
-				<input
-					type="text"
-					placeholder="Ingresa tu apellido"
-					onChange={(e) => {
-						setApellido(e.target.value);
-					}}
-					value={apellido}></input>
-				<button
-					className="btn btn-warning"
-					onClick={() => {
-						setNombre("");
-						setApellido("");
-					}}>
-					Limpiar input
-				</button>
-				<button
-					className="btn btn-success"
-					onClick={() => verificarNombre()}
-					onMouseOver>
-					Verificar
-				</button>
-				<button
-					className="btn btn-danger"
-					onClick={() => {
-						setLista(lista.concat(nombre));
-					}}>
-					Agregar a lista
-				</button>
+		<div className="row flex-fill vh-100">
+			<div className="col-3 bg-dark"></div>
+			<div className="col-6 bg-light">
+				<h1 className="text-center mt-3 todosSize">Todos</h1>
+				<div className="bg-light shadow p-3 mb-5" style={styleBox}>
+					<form
+						action=""
+						className="d-flex flex-row align-items-center"
+						onSubmit={agregarTarea}>
+						<input
+							type="text"
+							className="form-control mb-2 border-0 w-75 mx-auto"
+							placeholder="What needs to be done?"
+							onChange={(e) => setTarea(e.target.value)}
+							value={tarea}></input>
+					</form>
+					<ul className="list-group list-group-flush w-75 mx-auto">
+						{pendientes.map((item) => (
+							<li
+								className="list-group-item d-flex justify-content-between"
+								key={item.id}>
+								<span className="lead">{item.NombreTarea}</span>
+								<button
+									className="btn btn-sm btn-close float-right"
+									aria-label="Close"
+									onClick={() =>
+										eliminarTarea(item.id)
+									}></button>
+							</li>
+						))}
+					</ul>
+					<div>
+						<hr className="mb-1"></hr>
+						<p className="mt-0">{cantLista()} item left</p>
+					</div>
+				</div>
 			</div>
-			{lista.map((tarea) => {
-				return <h1>{tarea}</h1>;
-			})}
+			<div className="col-3 bg-dark"></div>
 		</div>
 	);
-};
+}
 
 export default Home;
