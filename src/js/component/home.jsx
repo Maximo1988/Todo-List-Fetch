254a602f6cc0def4503b72b96f0d71ce52f6b289
Maximo1988 <max_bd88@hotmail.com>
2022-03-10 22:13:33 +0000

    actualizando codigo y fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,23 +1,32 @@
 import React, { useState, useEffect } from "react";
-import { AgregarTarea } from "agregarTarea";
-
 
 const Home = () => {
 	const [tarea, setTarea] = useState("");
 	const [pendientes, setPendientes] = useState([]);
-	useEffect (()=>{
-		createData();
+	useEffect(() => {
+		createUser();
 		getData();
-		},[])
-	const getData = () => {
-	fetch("https://assets.breatheco.de/apis/fake/todos/user/Maximo1988", {
-	method: "GET",
-		headers: { "content-type": "application/json" },
-	})
-		.then((resp) => resp.json())
-		.then((data) => setPendientes(data))
-		.catch((err) => {console.log(err);
-		});
+	}, []);
+	const createUser = () => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/Maximo1988", {
+			method: "POST",
+			body: JSON.stringify([]),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((resp) => {
+				return resp.json();
+			})
+			.then((data) => {
+				//Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+				console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+			})
+			.catch((error) => {
+				//manejo de errores
+				console.log(error);
+			});
+	};
 	const subirData = (body) => {
 		const settingFetch = {
 			method: "PUT",
@@ -26,31 +35,42 @@ const Home = () => {
 				"Content-Type": "application/json",
 			},
 		};
-	}
 
-	fetch(
-		"https://assets.breatheco.de/apis/fake/todos/user/Maximo1988",
-		settingFetch
-	)
-	.then((respuesta) => respuesta.json())
-	.then((response) => (console.log(response))
-	);
-
-	const AgregarTarea = (e) => {
-		e.preventDefault();
-		if (!tarea.trim()) {
-			console.log("campo vacio");
-			return;
-		}
-
-		setPendientes([...pendientes, { id: AgregarTarea(), NombreTarea: tarea }]);
-
-		setTarea("");
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/Maximo1988",
+			settingFetch
+		)
+			.then((respuesta) => respuesta.json())
+			.then((response) => console.log(response));
 	};
 
-	function eliminarTarea(id) {
-		const arrayFilter = pendientes.filter((item) => item.id !== id);
+	const getData = () => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/Maximo1988", {
+			method: "GET",
+			headers: { "content-type": "application/json" },
+		})
+			.then((resp) => resp.json())
+			.then((data) => setPendientes(data))
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+	const AgregarTarea = (e) => {
+		e.preventDefault();
+		if (tarea == "") {
+			alert("el campo esta vacío");
+		} else {
+			setPendientes([...pendientes, { label: tarea, done: false }]);
+
+			setTarea("");
+			subirData([...pendientes, { label: tarea, done: false }]);
+		}
+	};
+
+	function eliminarTarea(task) {
+		const arrayFilter = pendientes.filter((item) => item.label !== task);
 		setPendientes(arrayFilter);
+		subirData(arrayFilter);
 	}
 
 	const cantLista = () => {
@@ -83,16 +103,16 @@ const Home = () => {
 							value={tarea}></input>
 					</form>
 					<ul className="list-group list-group-flush w-75 mx-auto">
-						{pendientes.map((item) => (
+						{pendientes.map((item, i) => (
 							<li
 								className="list-group-item d-flex justify-content-between"
-								key={item.id}>
-								<span className="lead">{item.NombreTarea}</span>
+								key={i}>
+								<span className="lead">{item.label}</span>
 								<button
 									className="btn btn-sm btn-close float-right"
 									aria-label="Close"
 									onClick={() =>
-										eliminarTarea(item.id)
+										eliminarTarea(item.label)
 									}></button>
 							</li>
 						))}
